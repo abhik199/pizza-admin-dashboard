0f66f47001572a636844c55d2e88df72b50ac69d
abhik199 <abhishekkirar2004@gmail.com>
2024-04-07 00:43:32 +0530

    add layout

--- src/router.tsx
+++ src/router.tsx
@@ -1,14 +1,28 @@
-import { createBrowserRouter } from 'react-router-dom'
-import HomePage from './pages/HomePage'
-import Login from './pages/login/Login'
+import { createBrowserRouter } from "react-router-dom";
+import HomePage from "./pages/HomePage";
+import Login from "./pages/login/Login";
+import Dashboard from "./layout/Dashboard";
+import NonAuth from "./layout/NonAuth";
 
 export const router = createBrowserRouter([
     {
-        path: '/',
-        element: <HomePage />
+        path: "/",
+        element: <Dashboard />,
+        children: [
+            {
+                path: "",
+                element: <HomePage />,
+            },
+        ],
     },
     {
-        path: '/auth/login',
-        element: <Login />
-    }
-])
+        path: "/auth",
+        element: <NonAuth />,
+        children: [
+            {
+                path: "login",
+                element: <Login />,
+            },
+        ],
+    },
+]);
