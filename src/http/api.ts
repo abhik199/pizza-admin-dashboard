4b26a0ee7cf1662f9af2dc9f0e43891d009df9ed
abhik199 <abhishekkirar2004@gmail.com>
2024-04-07 00:24:04 +0530

    logout

--- src/http/api.ts
+++ src/http/api.ts
@@ -1,4 +1,6 @@
-import { Credentials } from './../types';
+import { Credentials } from "./../types";
 import { api } from "./client";
-export const login = (credentials: Credentials) => api.post('/auth/login', credentials)
-export const self = () =>api.get("/auth/self")
+export const login = (credentials: Credentials) =>
+  api.post("/auth/login", credentials);
+export const self = () => api.get("/auth/self");
+export const logout = () => api.post("/auth/logout");
--- src/pages/login/Login.tsx
+++ src/pages/login/Login.tsx
@@ -1,97 +1,173 @@
-import { Layout, Card, Space, Form, Input, Checkbox, Button, Flex, Alert } from 'antd'
-import { LockFilled, UserOutlined, LockOutlined } from '@ant-design/icons'
+import {
+    Layout,
+    Card,
+    Space,
+    Form,
+    Input,
+    Checkbox,
+    Button,
+    Flex,
+    Alert,
+} from "antd";
+import { LockFilled, UserOutlined, LockOutlined } from "@ant-design/icons";
 // import { Link } from 'react-router-dom'
-import Logo from '../../components/icons/Logo'
-import { useMutation, useQuery } from '@tanstack/react-query'
-import { Credentials } from '../../types'
-import { login, self } from '../../http/api'
-import { useAuthStore } from '../../store'
-
+import Logo from "../../components/icons/Logo";
+import { useMutation, useQuery } from "@tanstack/react-query";
+import { Credentials } from "../../types";
+import { login, self, logout } from "../../http/api";
+import { useAuthStore } from "../../store";
+import { usePermission } from "../../hooks/usePermission";
 
 const loginUser = async (credentials: Credentials) => {
-    const { data } = await login(credentials)
-    return data
-}
-const getSelf = async () => {
-    const { data } = await self()
+    const { data } = await login(credentials);
     return data;
-}
+};
+const getSelf = async () => {
+    const { data } = await self();
+    return data;
+};
 
 const Login = () => {
-    const { setUser } = useAuthStore()
-    const { data: selfData, refetch } = useQuery({
-        queryKey: ['self'],
+    const { isAllowed } = usePermission();
+    const { setUser, logout: logoutFromStore } = useAuthStore();
+    const { refetch } = useQuery({
+        queryKey: ["self"],
         queryFn: getSelf,
-        enabled: false
-    })
+        enabled: false,
+    });
+    const { mutate: logoutMutate } = useMutation({
+        mutationKey: ["logout"],
+        mutationFn: logout,
+        onSuccess: async () => {
+            logoutFromStore();
+            return;
+        },
+
+        onError: async () => {
+            refetch();
+        },
+    });
     const { mutate, isPending, isError, error } = useMutation({
-        mutationKey: ['login'],
+        mutationKey: ["login"],
         mutationFn: loginUser,
         onSuccess: async () => {
             console.log("login");
             const selfDataPromise = await refetch();
+
+            if (!isAllowed(selfDataPromise.data)) {
+                logoutMutate();
+                return;
+            }
             setUser(selfDataPromise.data);
         },
         onError: async () => {
             console.log("login error");
-        }
-    })
+        },
+    });
     return (
         <>
-            <Layout style={{ height: '100vh', display: 'grid', placeItems: 'center' }}>
-                <Space direction='vertical' align='center' size='large'  >
-                    <Layout.Content style={{ display: 'flex', justifyContent: 'center', alignItems: 'center' }} >
+            <Layout
+                style={{ height: "100vh", display: "grid", placeItems: "center" }}
+            >
+                <Space direction="vertical" align="center" size="large">
+                    <Layout.Content
+                        style={{
+                            display: "flex",
+                            justifyContent: "center",
+                            alignItems: "center",
+                        }}
+                    >
                         <Logo />
                     </Layout.Content>
 
-                    <Card bordered={false} style={{ width: 300 }} title={<Space style={{ width: "100%", fontSize: 16, justifyContent: 'center' }} ><LockFilled />Sign in</Space>}  >
-                        <Form initialValues={{
-                            remember: true,
-                            username: 'demo@gmail.com',
-                            password: 'demo@123'
-                        }}
+                    <Card
+                        bordered={false}
+                        style={{ width: 300 }}
+                        title={
+                            <Space
+                                style={{
+                                    width: "100%",
+                                    fontSize: 16,
+                                    justifyContent: "center",
+                                }}
+                            >
+                                <LockFilled />
+                                Sign in
+                            </Space>
+                        }
+                    >
+                        <Form
+                            initialValues={{
+                                remember: true,
+                                username: "demo@gmail.com",
+                                password: "demo@123",
+                            }}
                             onFinish={(values) => {
-                                mutate({ email: values.username, password: values.password })
-                            }}>
-                            {isError && <Alert style={{ margin: 24 }} type='error' message={error.message} />}
-                            <Form.Item name="username" rules={[
-                                {
-                                    required: true,
-                                    message: 'Please enter a username'
-                                },
-                                {
-                                    type: 'email',
-                                    message: 'email is not valid',
-                                }
-                            ]}>
-                                <Input prefix={<UserOutlined />} placeholder='Username' />
+                                mutate({ email: values.username, password: values.password });
+                            }}
+                        >
+                            {isError && (
+                                <Alert
+                                    style={{ margin: 24 }}
+                                    type="error"
+                                    message={error.message}
+                                />
+                            )}
+                            <Form.Item
+                                name="username"
+                                rules={[
+                                    {
+                                        required: true,
+                                        message: "Please enter a username",
+                                    },
+                                    {
+                                        type: "email",
+                                        message: "email is not valid",
+                                    },
+                                ]}
+                            >
+                                <Input prefix={<UserOutlined />} placeholder="Username" />
                             </Form.Item>
-                            <Form.Item name="password" rules={[
-                                {
-                                    required: true,
-                                    message: "Please enter a password"
-                                }
-                            ]} >
-                                <Input.Password prefix={<LockOutlined />} placeholder='Password' />
+                            <Form.Item
+                                name="password"
+                                rules={[
+                                    {
+                                        required: true,
+                                        message: "Please enter a password",
+                                    },
+                                ]}
+                            >
+                                <Input.Password
+                                    prefix={<LockOutlined />}
+                                    placeholder="Password"
+                                />
                             </Form.Item>
-                            <Flex justify='space-between'>
-                                <Form.Item name="remember" valuePropName='checked'>
+                            <Flex justify="space-between">
+                                <Form.Item name="remember" valuePropName="checked">
                                     <Checkbox>Remember me</Checkbox>
                                 </Form.Item>
                                 {/* <Link id='login-form-forgot' to='/'>Forgot password</Link> */}
-                                <a href="" id='login-form-forgot' >Forgot password</a>
-
+                                <a href="" id="login-form-forgot">
+                                    Forgot password
+                                </a>
                             </Flex>
 
                             <Form.Item>
-                                <Button type='primary' htmlType='submit' style={{ width: '100%' }} loading={isPending} >Log in</Button>
+                                <Button
+                                    type="primary"
+                                    htmlType="submit"
+                                    style={{ width: "100%" }}
+                                    loading={isPending}
+                                >
+                                    Log in
+                                </Button>
                             </Form.Item>
                         </Form>
                     </Card>
                 </Space>
             </Layout>
         </>
-    )
-}
+    );
+};
 
-export default Login
+export default Login;
